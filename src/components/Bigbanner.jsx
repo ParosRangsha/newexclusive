1a882362fe68507445988eff3f5af0360d4caee5
Paros Rangsha <paroscrangsha@gmail.com>
2025-04-25 10:55:16 +0000

    Big ban add

--- src/components/Bigbanner.jsx
+++ src/components/Bigbanner.jsx
@@ -5,6 +5,7 @@ import { Apidata } from '../Mydata/ProductApi'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
+import {Link} from 'react-router-dom'
 
 const Bigbanner = () => {
     let info = useContext(Apidata)
@@ -24,11 +25,12 @@ const Bigbanner = () => {
                         <div className="banner flex bg-[#000] text-[#fff]" key={i}>
                             <div className="txt w-1/2 p-[48px]">
                                 <p>{item.category.toUpperCase()}</p>
-                                <div className="h-[150px]">
-                                    <p>{item.title.toUpperCase()}</p>
+                                <div className="h-[100px] py-[20px]">
+                                    <p className='text-[28px] font-bold'>{item.title.toUpperCase()}</p>
                                 </div>
+                                <p className="text-justify py-[20px]">{item.description}</p>
                                 <Timer duration={3 * 24 * 60 * 60 * 1000} />
-
+                                <Link to="/shop" className="inline-block py-[15px] px-[30px] bg-[#aa0] rounded-[5px]">Buy Now</Link>
                             </div>
                             <div className="thumb w-1/2 p-[48px]">
                                 <img src={item.thumbnail} alt={item.title}/>
