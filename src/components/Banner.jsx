d2cbe6314b6a2cefe538dcaae8656eb99be88119
sajibkhandev <sajibkhan.cit.bd@gmail.com>
2025-04-29 12:33:47 +0600

    cart add

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -25,48 +25,58 @@ const Banner = () => {
     
   return (
     <div className="banner">
-        <Container>
-            <div className="flex">
-                <div className="sidemenu py-[40px] pr-2 w-[15%] border-r-[1px]">
-                    <h1 className='font-bold text-[18px]'>Shop by Category</h1>
-                    <ul className='flex flex-col py-[20px] h-[500px] overflow-y-scroll'>
-                        {
-                            category.map((item, i)=>(
-                                <li key={i} className='py-[5px] flex justify-between items-center hover:bg-[#db4444] hover:text-[#fff] duration-[1s] hover:px-[15px] cursor-pointer'>{item.slice(0,10).toUpperCase()}<IoIosArrowForward/></li>
-                            ))
-                        }
-                    </ul>
+      <Container>
+        <div className="flex">
+          <div className="sidemenu py-[40px] pr-2 w-[15%] border-r-[1px]">
+            <h1 className="font-bold text-[18px]">Shop by Category</h1>
+            <ul className="flex flex-col py-[20px] h-[500px] overflow-y-scroll">
+              {category.map((item, i) => (
+                <li key={i}>
+                  <Link to='/shop' className="py-[5px] flex justify-between items-center hover:bg-[#db4444] hover:text-[#fff] duration-[1s] hover:px-[15px] cursor-pointer">
+                    {item.slice(0, 10).toUpperCase()}
+                    <IoIosArrowForward />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="ban w-[85%] pt-[40px] pl-[40px] pb-[40px]">
+            <Slider {...banslider}>
+              {info.map((item, i) => (
+                <div className="">
+                  <div className="slideitem w-full bg-[#000] text-[#fff] flex">
+                    <div className="sidetext w-1/2 p-[58px]">
+                      <div className="title flex gap-3 items-center">
+                        <img src={item.thumbnail} alt="" className="h-[50px]" />
+                        <h1 className="text-[20px]">{item.title}</h1>
+                      </div>
+                      <h1 className="text-[48px] font-semibold">
+                        Up to {Math.floor(item.discountPercentage)}% off Voucher
+                      </h1>
+                      <div className="flex items-center gap-3 py-[10px]">
+                        <Link
+                          to="/shop"
+                          className='inline-block items-center gap-3 relative after:absolute after:contents-[""] after:h-[3px] after:w-full after:bg-[#fff] after:top-full after:left-0 bg-[#333] hover:bg-[#555] py-[10px] px-[15px] duration-[1s]'
+                        >
+                          Shop Now{" "}
+                        </Link>
+                      </div>
+                      <p className="text-justify h-[100px]">
+                        {item.description}
+                      </p>
+                    </div>
+                    <div className="thumb w-1/2 p-[48px]">
+                      <img src={item.thumbnail} alt="" className="w-full" />
+                    </div>
+                  </div>
                 </div>
-                <div className="ban w-[85%] pt-[40px] pl-[40px] pb-[40px]">
-                    <Slider {...banslider}>
-                    {
-                        info.map((item, i)=>(
-                                <div className="">
-                                    <div className="slideitem w-full bg-[#000] text-[#fff] flex">
-                                        <div className="sidetext w-1/2 p-[58px]">
-                                            <div className="title flex gap-3 items-center">
-                                                <img src={item.thumbnail} alt="" className='h-[50px]'/>
-                                                <h1 className='text-[20px]'>{item.title}</h1>
-                                            </div>
-                                            <h1 className='text-[48px] font-semibold'>Up to {Math.floor(item.discountPercentage)}% off Voucher</h1>
-                                            <div className="flex items-center gap-3 py-[10px]">
-                                                <Link to='/shop' className='inline-block items-center gap-3 relative after:absolute after:contents-[""] after:h-[3px] after:w-full after:bg-[#fff] after:top-full after:left-0 bg-[#333] hover:bg-[#555] py-[10px] px-[15px] duration-[1s]'>Shop Now </Link> 
-                                            </div>
-                                            <p className='text-justify h-[100px]'>{item.description}</p>
-                                        </div>
-                                        <div className="thumb w-1/2 p-[48px]">
-                                            <img src={item.thumbnail} alt="" className="w-full"/>
-                                        </div>
-                                    </div>
-                                </div>
-                        ))
-                    }
-                    </Slider>
-                </div>
-            </div>
-        </Container>
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
 export default Banner
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,75 +2,87 @@ import React, {useContext, useState, useEffect} from 'react'
 import { Apidata } from '../Mydata/ProductApi'
 import { FaRegHeart } from "react-icons/fa";
 import { BiShow } from "react-icons/bi";
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from "react-toastify";
 
 
 const Post = ({showData, allapge}) => {
     let info = useContext(Apidata)
-    
+    let navigate = useNavigate()
+    let handleCart = ()=>{
+        toast('hello')
+        setTimeout(() => {
+            navigate("/cart");
+        }, 3000);
+    }
   return (
     <div>
-        <div className="heading w-full bg-[#db4444] py-[10px]">
-            a
-        </div>
-        <div className="products">
-            {
-                showData.length > 0 ? 
-                <div className="flex flex-wrap">
-                {
-                    showData.map((item, i)=>(
-                        <div className="items w-1/4 px-[5px] py-[20px]">
-                            <div className="thumb p-[12px] relative bg-[#ddd] group">
-                                <img src={item.thumbnail} alt="" />
-                                <div className="offer absolute top-[12px] left-[12px] py-[4px] px-[12px] bg-[#DB4444] text-[#fff] rounded-[5px]">
-                                    <p>-{Math.floor(item.discountPercentage)}%</p>
-                                </div>
-                                <div className="btns absolute top-[12px] right-[12px] flex flex-col gap-3 text-[22px]">
-                                    <FaRegHeart/>
-                                    <Link to={`/shop/${item.id}`}><BiShow/></Link>
-                                </div>
-                                <div className="addtocart absolute bottom-0 left-0 w-full scale-0 group-hover:scale-[100%] duration-[1s] bg-[#000] text-[#fff] py-[10px]">
-                                    <p className='text-center'>Add To Cart</p>
-                                </div>
-                            </div>
-                            <div className="txt py-[5px]">
-                                <p className='font-bold text-[18px]'>{item.title.slice(0,20)}</p>
-                                <p>Price: ${item.price}</p>
-                            </div>
-                        </div>
-                    ))
-                }
+      <div className="heading w-full bg-[#db4444] py-[10px]">a</div>
+      <div className="products">
+        {showData.length > 0 ? (
+          <div className="flex flex-wrap">
+            {showData.map((item, i) => (
+              <div className="items w-1/4 px-[5px] py-[20px]">
+                <div className="thumb p-[12px] relative bg-[#ddd] group">
+                  <img src={item.thumbnail} alt="" />
+                  <div className="offer absolute top-[12px] left-[12px] py-[4px] px-[12px] bg-[#DB4444] text-[#fff] rounded-[5px]">
+                    <p>-{Math.floor(item.discountPercentage)}%</p>
+                  </div>
+                  <div className="btns absolute top-[12px] right-[12px] flex flex-col gap-3 text-[22px]">
+                    <FaRegHeart />
+                    <Link to={`/shop/${item.id}`}>
+                      <BiShow />
+                    </Link>
+                  </div>
+                  <ToastContainer />
+                  <div
+                    onClick={handleCart}
+                    className="addtocart absolute bottom-0 left-0 w-full scale-0 group-hover:scale-[100%] duration-[1s] bg-[#000] text-[#fff] py-[10px]"
+                  >
+                    <p className="text-center">Add To Cart</p>
+                  </div>
                 </div>
-                :
-                <div className="flex flex-wrap">
-                {
-                    allapge.map((item, i)=>(
-                        <div className="items w-1/4 px-[5px] py-[20px]" key={i}>
-                            <div className="thumb p-[12px] relative bg-[#ddd] group">
-                                <img src={item.thumbnail} alt="" />
-                                <div className="offer absolute top-[12px] left-[12px] py-[4px] px-[12px] bg-[#DB4444] text-[#fff] rounded-[5px]">
-                                    <p>-{Math.floor(item.discountPercentage)}%</p>
-                                </div>
-                                <div className="btns absolute top-[12px] right-[12px] flex flex-col gap-3 text-[22px]">
-                                    <FaRegHeart/>
-                                    <Link to={`/shop/${item.id}`}><BiShow/></Link>
-                                </div>
-                                <div className="addtocart absolute bottom-0 left-0 w-full scale-0 group-hover:scale-[100%] duration-[1s] bg-[#000] text-[#fff] py-[10px]">
-                                    <p className='text-center'>Add To Cart</p>
-                                </div>
-                            </div>
-                            <div className="txt py-[5px]">
-                                <p className='font-bold text-[18px]'>{item.title.slice(0,20)}</p>
-                                <p>Price: ${item.price}</p>
-                            </div>
-                        </div>
-                    ))
-                }
+                <div className="txt py-[5px]">
+                  <p className="font-bold text-[18px]">
+                    {item.title.slice(0, 20)}
+                  </p>
+                  <p>Price: ${item.price}</p>
                 </div>
-            }
-        </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-wrap">
+            {allapge.map((item, i) => (
+              <div className="items w-1/4 px-[5px] py-[20px]" key={i}>
+                <div className="thumb p-[12px] relative bg-[#ddd] group">
+                  <img src={item.thumbnail} alt="" />
+                  <div className="offer absolute top-[12px] left-[12px] py-[4px] px-[12px] bg-[#DB4444] text-[#fff] rounded-[5px]">
+                    <p>-{Math.floor(item.discountPercentage)}%</p>
+                  </div>
+                  <div className="btns absolute top-[12px] right-[12px] flex flex-col gap-3 text-[22px]">
+                    <FaRegHeart />
+                    <Link to={`/shop/${item.id}`}>
+                      <BiShow />
+                    </Link>
+                  </div>
+                  <div className="addtocart absolute bottom-0 left-0 w-full scale-0 group-hover:scale-[100%] duration-[1s] bg-[#000] text-[#fff] py-[10px]">
+                    <p className="text-center">Add To Cart</p>
+                  </div>
+                </div>
+                <div className="txt py-[5px]">
+                  <p className="font-bold text-[18px]">
+                    {item.title.slice(0, 20)}
+                  </p>
+                  <p>Price: ${item.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Post
